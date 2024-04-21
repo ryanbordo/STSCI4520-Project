18bec638fa25be3d348672489bf85af32b210b81
reb376 <reb376@cornell>
2024-04-21 14:44:15 -0400

    fixed time_series

--- ProjectSTSCI4520/R/time_series.R
+++ ProjectSTSCI4520/R/time_series.R
@@ -1,7 +1,7 @@
-load(daily_weather)
+load("../data/daily_weather.RData")
 
-#assuming station id is WBANNO
-get_station_data(station_id, start_date = "2000-01-01", end_date = "3000-01-01"){
-  cond = daily_weather$WBANNO == station_id & daily_weather$LST_DATE <= end_date & daily_weather$LST_DATE >= start_date
+# assuming station id is WBANNO
+get_station_data = function(station_id, start_date = "2000-01-01", end_date = "3000-01-01"){
+  cond = (daily_weather$WBANNO == station_id) & (daily_weather$LST_DATE <= end_date) & (daily_weather$LST_DATE >= start_date)
   return (daily_weather[cond, ])
 }
