125bdc45b6b12dd34734f4cc84460eeee9053114
unknown <cameronxu12@gmail.com>
2024-05-06 20:14:39 -0400

    fixed item glitch

--- package/ProjectSTSCI4520/R/plotting.R
+++ package/ProjectSTSCI4520/R/plotting.R
@@ -49,11 +49,11 @@ create_grid <- function(resolution_X = 50,
 #' @param gridpoints a series of sf points associated to the grid points to be interpolated with.
 #' @param use_elev a logical of whether or not to calculate and use elevation for interpolations
 #' @return a dataframe containing the following columns:
-#' \describe{
-#'   \item {interpolations}{The numeric interpolated data points to plotted}
-#'   \item {longitudes}{A numeric of the longitudes to plot}
-#'   \item {latitudes}{A numeric of the latitudes to plot}
-#'   \item {inUSA}{A binary variable whether or not to plot (within contiguous USA)}
+#' \itemize{
+#'   \item \code{interpolations}:   {The numeric interpolated data points to plotted}
+#'   \item \code{longitudes}:  {A numeric of the longitudes to plot}
+#'   \item \code{latitudes}:  {A numeric of the latitudes to plot}
+#'   \item \code{inUSA}:   {A binary variable whether or not to plot (within contiguous USA)}
 #'   }
 #' @examples
 #' Interpolates a plot to the daily average temperature across the US
